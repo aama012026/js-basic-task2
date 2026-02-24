11f5cf26b167bebacbda3624f4bbbd2c4825f0f6
aama012026 <aama012026@gmail.com>
2026-02-24 14:51:03 +0100

    Finished tasks.

--- tasks.js
+++ tasks.js
@@ -21,7 +21,99 @@ funksjonen)
 
 ******************************************************************************/
 
-// Skriv koden for oppgave 1 her
+function isOdd(integer) {
+	const rest = integer % 2;
+	switch (rest) {
+		case 0:
+			return `${integer} is even.`;
+		case 1:
+			return `${integer} is odd.`;
+		default: // Brukar brukaren funksjonen riktig?
+			// Om koden når her er det ein bug i brukar-kontekst, og bør derfor
+			// kastes som ein feil! Vi veit heller ikkje korleis brukaren ønsker
+			// å handtere feilen, så det er opp til brukaren å sjekke. 
+			throw new TypeError(`isOdd expects an integer, but ${integer} was passed in!`);
+	}
+}
+
+console.log(isOdd(4));
+
+// Eksempel på brukarhandtering
+function isOddSafe(integer, logLevel = 'verbose') {
+	let result = null;
+	try {
+		result = isOdd(integer);
+	}
+	catch (error) {
+		if (logLevel === 'error') {
+			console.log(error);
+		}
+	}
+	finally {
+		switch (logLevel) {
+			case 'silent':
+			case 'error':
+				break;
+			case 'verbose':
+			default:
+				console.log(result);
+				break;
+		}
+	}
+	// MERK: vi sender att null om ein feil blir kasta. Dette er ønska då vi
+	// ikkje vil at nokon skal nytte resultatet om det er feil.
+	return result;
+}
+
+const resultString = isOddSafe(33, 'silent');
+isOddSafe(78);
+isOddSafe(57.32, 'error');
+console.log('No kan vi kaste feil uten at programmet vårt krasjer!');
+
+// throw med try...catch..finally er den skikkelege måten å handtere feil på,
+// men sjå kor mykje vi måtte skrive. I tillegg blir ofte feil berre kasta, men
+// ikkje fanga. Dette peker eigentleg på at ein har døme ein ikkje har, men
+// burde ta stilling til i programmet, men kven har lyst å skrive meir
+// feilhandtering- enn programkode? I JS er det vanleg å sende att null i
+// staden, i og med at ein kan blande typer. Dette er ein av grunnene for å ha
+// fleire tomme verdier - null er gitt av brukar, undefined (bør vere) av
+// språket.
+
+// Ein moderne handtering av feil er å gå tilbake til ein eldre handtering.
+// Her sender vi att ein ok/feilverdi i tillegg til resultatet. Dette er
+// meir ergonomisk i nokre språk enn andre, spesielt med unionar, men eg gir
+// eit enkelt js eksempel under:
+
+function isEven(int) {
+	const rest = int % 2;
+	switch (rest) {
+		case 0:
+			return { result: true, ok: true };
+		case 1:
+			return { result: false, ok: true };
+		default: // Brukar brukaren funksjonen riktig?
+			return {
+				result: `isEven expects an integer, but was given ${int}!`,
+				ok: false
+			};
+	}
+}
+
+// Brukaren held på all fleksibilitet i handtering, men kan få meir info enn
+// berre null. Brukaren blir og gjort merksam på at ein må handtere ein mogleg
+// feil pga datatypen funksjonen sender att.
+const anEvenCheck = isEven(44).ok ? isEven(44).result : null;
+
+// I mange språk kan ein òg gjere one-lineren utan å kalle funksjonen 2 gonger.
+// Det ser ikkje såå bra ut i js, men ein kan nytte IIFE:
+const anotherCheck = ((r) => r.ok ? r.result : null)(isEven(28));
+console.log(`Is 28 even? ${anotherCheck}`);
+
+// Og det kan vere greit i døme der du veit at verdien du gir inn er god, eller
+// du vil krasje programmet om feilen skjer, men ein enklere og meir fleksibel
+// løysing viser òg kva vi tjener på tilnærminga over null checks:
+const badCheck = isEven(67.76);
+if (!badCheck.ok) console.log(badCheck.result);
 
 /******************************************************************************
 2.
@@ -35,7 +127,8 @@ Eksempel: "Dette er kult" skal returnere "DETTE ER KULT!"
 
 ******************************************************************************/
 
-// Skriv koden for oppgave 2 her
+const makeExclamation = (string) => string.toUpperCase() + '!';
+console.log(makeExclamation('Dette er ein pilfunksjon'));
 
 /******************************************************************************
 3.
@@ -59,7 +152,49 @@ Hvis ingen timeverdi mottas, skal funksjonen returnere en feilmelding.
 
 ******************************************************************************/
 
-// Skriv koden for oppgave 3 her
+function greetTimely(name, hourOf23) {
+	const result = {
+		greeting: null,
+		err: `Ufullendt utføring av funksjon.`
+	}
+	// Tidleg retur.
+	if (!hourOf23 && hourOf23 != 0) {
+		result.err = `No hour specified.`;
+		return result;
+	}
+	// switch (true) lar oss skrive uttrykk som matcher om sann. 
+	switch (true) {
+		case hourOf23 < 0:
+			result.err = `hourOf23 må vere eit positivt tal! Fekk: ${hourOf23}`;
+			break;
+		case hourOf23 < 6:
+			result.greeting = `God natt ${name}`, result.err = false;
+			break;
+		case hourOf23 < 12:
+			result.greeting = `God morgon ${name}`, result.err = false;
+			break;
+		case hourOf23 < 18:
+			result.greeting = `God dag ${name}`, result.err = false;
+			break;
+		case hourOf23 < 24: 
+			result.greeting = `God kveld ${name}`, result.err = false;
+			break;
+		case hourOf23 > 23: 
+			result.err = `hourOf23 kan ikkje være høgare enn 23! Fekk: ${hourOf23}`;
+			break;
+		default:
+			result.err = `Noko gjekk gale - switch defaulted. Switch: ${hourOf23}`;
+			break;
+	}
+	return result;
+}
+
+console.log(((r) => r.err ? r.err : r.greeting)(greetTimely('Rettleiar', 0)));
+console.log(((r) => r.err ? r.err : r.greeting)(greetTimely('Rettleiar', -1)));
+
+const greeting = greetTimely('Vrangleiar', 24);
+const greetMsg = greeting.err ? greeting.err : greeting.result;
+console.log(greetMsg);
 
 /******************************************************************************
 4.
@@ -76,7 +211,12 @@ Eksempel 2: ["En", "To", "Tre", "Fire", "Fem", "Seks"] skal returnere
 
 ******************************************************************************/
 
-// Skriv koden for oppgave 4 her
+const truncateArray = (a) => a.slice(1, -1);
+
+const initialArray = [0, 4, 3, 2, 78, true, 'hei'];
+const shortArray = truncateArray(initialArray);
+console.log(initialArray);
+console.log(shortArray);
 
 /******************************************************************************
 5.
@@ -97,7 +237,13 @@ Eksempel 3: "   vanskelig        " skal returnere "gøy".
 
 ******************************************************************************/
 
-// Skriv koden for oppgave 5 her
+function strictFun(strParam) {
+	return strParam.replaceAll("vanskelig","gøy").trim();
+}
+
+console.log(strictFun('  JS er vanskelig  '));
+console.log(strictFun(' Det er vanskelig å bruke metoder når det er vanskelig, vanskelig'));
+console.log(strictFun('		vanskelig    		 '));
 
 /******************************************************************************
 6.
@@ -122,7 +268,28 @@ Ekstra utfordring: Lag et nytt array som kun inkluderer elementer som inneholder
 
 ******************************************************************************/
 
-// Skriv koden for oppgave 6 her
+items.shift();
+console.log(items);
+
+const i = items.indexOf('Viskelær');
+if (i) {
+	items[i] = 'Linjal';
+}
+console.log(items);
+
+// Litt meir fluff
+const pennPos = items.indexOf('Penn');
+const bokPos = items.indexOf('Notatbok');
+if (pennPos >= 0 && bokPos >= 0 ) {
+	const startPos = pennPos < bokPos ? pennPos : bokPos;
+	items.splice(startPos, Math.abs(pennPos-bokPos), "Markeringspenn");
+}
+console.log(items);
+
+console.log(items.join(' | '));
+
+const eItems = items.filter((i) => i.includes('e'));
+console.log(eItems);
 
 /******************************************************************************
 7.
@@ -148,7 +315,23 @@ Eksempel 4: (["En", "To", "Tre"], "To") --> ["En", "Tre"]
 
 ******************************************************************************/
 
-// Skriv koden for oppgave 7 her
+function addOrRemove(string, array) {
+	const i = array.indexOf(string);
+	i === -1 ? array.push(string) : array.splice(i, 1);
+}
+
+const fargar = ['Rød', 'Grønn'];
+const tall = ['En', 'To', 'Tre'];
+
+addOrRemove('Blå', fargar);
+console.log(fargar);
+addOrRemove('Blå', fargar);
+console.log(fargar);
+
+addOrRemove('Fire', tall);
+console.log(tall);
+addOrRemove('To', tall);
+console.log(tall);
 
 /******************************************************************************
 8.
@@ -177,3 +360,22 @@ Returner "😎Kun primitive verdier😎".
 ******************************************************************************/
 
 // Skriv koden for oppgave 8 her
+
+function coolTypeOf(primitive) {
+	const type = typeof(primitive);
+	switch (type) {
+		case "string":
+			return `😎${primitive}😎`;
+		case "number":
+			return `😎${primitive * 2}😎`;
+		case "boolean":
+			return `😎${primitive ? 'Ja' : 'Slapp av'}😎`;
+		default:
+			return '😎Kun primitive verdier😎';
+	}
+}
+
+console.log(coolTypeOf('hallo'));
+console.log(coolTypeOf(21));
+console.log(coolTypeOf(false));
+console.log(coolTypeOf(null));
